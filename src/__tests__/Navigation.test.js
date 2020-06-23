6255ef762c462a136067aac33cde54640a8bb193
FayeCarter <fayecarter333@gmail.com>
2020-06-23 14:55:02 +0100

    refactor navigation tests

--- src/__tests__/Navigation.test.js
+++ src/__tests__/Navigation.test.js
@@ -4,13 +4,15 @@ import Navigation from "../components/Navigation";
 import { shallow } from 'enzyme';
 
 describe("Home page testing", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Navigation />);
+  })
   test("renders with `Home` link", () => {
-    const wrapper = shallow(<Navigation />)
     expect(wrapper.find(".home-button").text()).toContain("Home");
   });
 
   test("`Home` link navigates to Home page", () => {
-    const wrapper = shallow(<Navigation />)
     wrapper.find(".home-button").simulate('click');
     expect(window.location.pathname).toBe("/");
   });
