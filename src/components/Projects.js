fee61e402339354247710d7d7a6c451c6190d715
FayeCarter <fayecarter333@gmail.com>
2020-06-23 17:48:53 +0100

    add basic projects css

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Navigation from './Navigation';
 import Project from "./Project";
+import './Projects.css';
 
 function Projects() {
   return (
     <div className="Projects">
       <Navigation />
       <h1>Projects</h1>
-      <Project 
-        title="WhatStack"
-        project_description="A welcoming chat room environment for developers."
-        tech_stack="Sockets, OAuth, React, Node, Express, MongoDB"
-        image="https://github.com/FayeCarter/CV/raw/master/gifs/WhatStack.gif"
-      />
+      <div className="project-container">
+        <Project 
+          title="WhatStack"
+          project_description="A welcoming chat room environment for developers."
+          tech_stack="Sockets, OAuth, React, Node, Express, MongoDB"
+          image="https://github.com/FayeCarter/CV/raw/master/gifs/WhatStack.gif"
+        />
+      </div>
     </div>
   );
 }
