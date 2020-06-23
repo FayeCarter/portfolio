c3609c0d211a91b2ecb755b2becf0dcd176dbb4e
FayeCarter <fayecarter333@gmail.com>
2020-06-23 14:20:02 +0100

    move homepage elemnts to Hom component

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,11 +1,11 @@
 import React from "react";
 import App from "../App";
 
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 describe("App testing", () => {
   test("renders with `Faye Carter` title", () => {
-    const wrapper = shallow(<App />)
+    const wrapper = mount(<App />)
     expect(wrapper.find("h1").text()).toContain("Faye Carter");
   });
 })
