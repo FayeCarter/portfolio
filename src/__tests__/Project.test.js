adb70751f85b04f07fb3fb35d398309c8325f594
FayeCarter <fayecarter333@gmail.com>
2020-06-23 15:34:21 +0100

    project renders image with passed src

--- src/__tests__/Project.test.js
+++ src/__tests__/Project.test.js
@@ -6,16 +6,20 @@ import { shallow } from 'enzyme';
 describe("Project page", () => {
   let wrapper;
 
-  beforeEach(() => {
-    wrapper = shallow(<Project title="Project Name" project_description="Project Description"/>);
-  })
-
   test("has a passed title `Project Name`", () => {
+    wrapper = shallow(<Project title="Project Name" />)
     expect(wrapper.find("h1").text()).toContain("Project Name");
   });
 
   test("has a passed project description `Project Description`", () => {
+    wrapper = shallow(<Project project_description="Project Description"/>)
     expect(wrapper.find(".project-description").text()).toContain("Project Description");
+  });
+
+  test("has a passed image source `Project Description`", () => {
+    const img = "shorturl.at/ryMW1"
+    wrapper = shallow(<Project image={img} />)
+    expect(wrapper.find(".project-image").prop("src")).toBe(img);
   });
 
 })
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-function Project( { title, project_description } ) {
+function Project( { title, project_description, image } ) {
   return (
     <div className="Project">
       <h1>{ title }</h1>
       <div className="project-description">{ project_description }</div>
+      <img className="project-image" src={ image } alt="project-image"/>
     </div>
   );
 }
