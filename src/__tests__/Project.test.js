307ba7952f9f518b15bab751d059cb1a9bcc0368
FayeCarter <fayecarter333@gmail.com>
2020-06-23 15:27:31 +0100

    project renders passed project description

--- src/__tests__/Project.test.js
+++ src/__tests__/Project.test.js
@@ -7,11 +7,15 @@ describe("Project page", () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<Project title="Project Name" />);
+    wrapper = shallow(<Project title="Project Name" project_description="Project Description"/>);
   })
 
   test("has a passed title `Project Name`", () => {
     expect(wrapper.find("h1").text()).toContain("Project Name");
+  });
+
+  test("has a passed project description `Project Description`", () => {
+    expect(wrapper.find(".project-description").text()).toContain("Project Description");
   });
 
 })
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-function Project( { title } ) {
+function Project( { title, project_description } ) {
   return (
     <div className="Project">
       <h1>{ title }</h1>
+      <div className="project-description">{ project_description }</div>
     </div>
   );
 }
