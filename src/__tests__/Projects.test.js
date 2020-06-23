5e09271f4aad7998719df496c45d7205dfa090cc
FayeCarter <fayecarter333@gmail.com>
2020-06-23 15:40:19 +0100

    add Whatstack to projects page

--- src/__tests__/Projects.test.js
+++ src/__tests__/Projects.test.js
@@ -1,19 +1,26 @@
 import React from "react";
 import Projects from "../components/Projects";
 import Navigation from "../components/Navigation";
+import Project from "../components/Project";
 
 import { shallow } from 'enzyme';
 
 describe("Projects page", () => {
   let wrapper;
+
   beforeEach(() => {
     wrapper = shallow(<Projects />);
   })
+  
   test("renders with `Projects` title", () => {
     expect(wrapper.find("h1").text()).toContain("Projects");
   });
 
   test("renders with Navigation", () => {
     expect(wrapper.containsMatchingElement(<Navigation />)).toEqual(true);
+  });
+
+  test("renders with WhatStack project", () => {
+    expect(wrapper.containsMatchingElement(<Project title="WhatStack" />)).toEqual(true);
   });
 })
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Navigation from './Navigation';
+import Project from "./Project";
 
 function Projects() {
   return (
     <div className="Projects">
       <Navigation />
       <h1>Projects</h1>
+      <Project 
+        title="WhatStack"
+        project_description="The aim of the project was to create a welcoming chat room environment for developers to ask for help.
+        Users can access the site using their GitHub Login details, create a room based around their concern, post a question and receive feedback in real-time.
+        If the user wants to send a code block, it can be entered using markdown by typing three backticks followed by the language their code is written in. The message will be formatted as a code block in the specified language highlighting variables and correctly indenting each line."
+        image="https://github.com/FayeCarter/CV/raw/master/gifs/WhatStack.gif"
+      />
     </div>
   );
 }
