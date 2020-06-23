a3f212a9c6f0731e51538d753442492f45e80908
FayeCarter <fayecarter333@gmail.com>
2020-06-23 14:21:44 +0100

    add subheading to Home

---
+++ src/__tests__/Home.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from "../components/Home";
+
+import { shallow } from 'enzyme';
+
+describe("Home page testing", () => {
+  test("renders with `Faye Carter` title", () => {
+    const wrapper = shallow(<Home />)
+    expect(wrapper.find("h1").text()).toContain("Faye Carter");
+  });
+
+  test("renders with `Junior Full Stack Developer` subheading", () => {
+    const wrapper = shallow(<Home />)
+    expect(wrapper.find("h2").text()).toContain("Junior Full Stack Developer");
+  });
+})
