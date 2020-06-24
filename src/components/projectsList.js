8969d0bd40290f1863a25db09419a8bd7c771b68
FayeCarter <fayecarter333@gmail.com>
2020-06-24 15:19:54 +0100

    add project description to Project details page

--- src/components/projectsList.js
+++ src/components/projectsList.js
@@ -3,8 +3,8 @@ const projectsList = [
     "title": "WhatStack",
     "short-description": "A welcoming chat room environment for developers.",
     "full-description": [
-      "The aim of the project was to create a welcoming chat room environment for developers to ask for help.",
-      "Users can access the site using their GitHub Login details, create a room based around their concern, post a question and receive feedback in real-time.",
+      "The aim of the project was to create a welcoming chat room environment for developers to ask for help.\n",
+      "Users can access the site using their GitHub Login details, create a room based around their concern, post a question and receive feedback in real-time.\n",
       "If the user wants to send a code block, it can be entered using markdown by typing three backticks followed by the language their code is written in. The message will be formatted as a code block in the specified language highlighting variables and correctly indenting each line.",
     ],
     "tech-stack": "Sockets, OAuth, React, Node, Express, MongoDB",
