25f8a8352c110bd53ed4e1991b90bb5e3d644015
FayeCarter <fayecarter333@gmail.com>
2020-06-24 14:01:20 +0100

    refactor project details to get project title from projectList

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -1,14 +1,21 @@
-import React, {useEffect} from 'react';
-
+import React, { useEffect, useState } from 'react';
+import projectsList from "./projectsList";
 
 function ProjectDetails( { match } ) {
+  const [ projectInfo, setProjectInfo ] = useState({})
 
-  useEffect(() => [
-    console.log({match})
-  ])
+  const getProject = () => {
+    const project = projectsList.find((project) => project["title"] ===  match.params.name )
+    setProjectInfo(project)
+  }
+
+  useEffect(() => {
+    getProject()
+  })
+
   return (
     <div className="ProjectDetails">
-      <div className="project-title">{ match.params.name }</div>
+      <div className="project-title">{ projectInfo.title }</div>
     </div>
   );
 }
