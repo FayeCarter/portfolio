46d118d34aa3bca104a3e78ed539f4f8460b4753
FayeCarter <fayecarter333@gmail.com>
2020-06-25 11:53:14 +0100

    update Navigation text

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,11 +7,10 @@ function Navigation() {
     <div className="Navigation">
       <ul>
           <Link to="/">
-            <li className="home-button">Home</li>
+            <li className="home-button">HOME</li>
           </Link>
-            <li> /  </li>
           <Link to="/projects">
-            <li className="projects-button">Projects</li>
+            <li className="projects-button">PROJECTS</li>
           </Link>
       </ul>
     </div>
