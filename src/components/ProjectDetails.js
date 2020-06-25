a3ab847adf1c464cf15f6db7bc69d66f472e6679
FayeCarter <fayecarter333@gmail.com>
2020-06-25 14:46:34 +0100

    Remove Link import from projectdetails

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import Navigation from './Navigation';
 import projectsList from "./projectsList";
 import './ProjectDetails.css';
