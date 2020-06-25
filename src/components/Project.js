35ffb15d727eeb71927d54ef3afbe751239ecb60
FayeCarter <fayecarter333@gmail.com>
2020-06-25 12:14:21 +0100

    switch Link to be inside Project contatiner

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,14 +5,14 @@ import './Project.css';
 
 function Project( { title, project_description, tech_stack, image } ) {
   return (
-    <Link to={`projects/${title}`}>
-      <div className="Project">
+    <div className="Project">
+      <Link to={`projects/${title}`}>
         <img className="project-image" src={ image } alt={title} />
         <div className="project-title">{ title }</div>
         <div className="project-description">{ project_description }</div>
         <div className="tech-stack">{ tech_stack }</div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 }
 
