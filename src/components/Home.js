8cfee489ea0e9daafa95dc4de2802b1ce9f95554
FayeCarter <fayecarter333@gmail.com>
2020-06-25 16:51:05 +0100

    align links

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,16 +5,18 @@ import './Home.css';
 
 function Home() {
   return (
-    <div className="Window">
-      <div className="Home">
-        <Navigation />
-        <Links />
-        <div className="home-details">
-          <h1 className="name" >Faye Carter</h1>
-          <h2>Junior Full Stack Developer</h2>
+    <div> 
+      <Links />
+        <div className="Window">
+        <div className="Home">
+          <Navigation />
+          <div className="home-details">
+            <h1 className="name" >Faye Carter</h1>
+            <h2>Junior Full Stack Developer</h2>
+          </div>
         </div>
       </div>
-    </div>
+    </div>  
   );
 }
 
