1c31d0500a7549a81d076be0cb5454e79d192f22
FayeCarter <fayecarter333@gmail.com>
2020-06-25 16:32:49 +0100

    orientate links to bottom left corner

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './Links.css';
 
 function Links() {
   return (
     <div className="Links">
-      <ul>
+      <ul className="links-list">
         <a className="github" href="https://github.com/FayeCarter" target="_blank" rel="noopener noreferrer">
-          <li>GITHUB</li>
+          <li className="media-link" >GITHUB</li>
         </a>
         <a className="linkedin" href="https://www.linkedin.com/in/faye-carter-74882bab/" target="_blank" rel="noopener noreferrer">
-          <li>LINKEDIN</li>
+          <li className="media-link" >LINKEDIN</li>
         </a>
       </ul>
     </div>
