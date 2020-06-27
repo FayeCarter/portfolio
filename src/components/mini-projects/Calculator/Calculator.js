f8c59ffdd1f1eb71b3bb43372683ad4c12a0cd06
FayeCarter <fayecarter333@gmail.com>
2020-06-27 13:21:07 +0100

    set number values as const

--- src/components/mini-projects/Calculator/Calculator.js
+++ src/components/mini-projects/Calculator/Calculator.js
@@ -1,3 +1,5 @@
+const NUMBERS = ["1","2","3","4","5","6","7","8","9","0"]
+
 import React, { useState } from 'react';
 
 function Calculator(value) {
@@ -17,7 +19,7 @@ function Calculator(value) {
         <div className="calc-result" >0</div>
         <div className="calc-input" >{ display }</div>
         <div className="numbers">
-          {["1","2","3","4","5","6","7","8","9","0"].map((number, index) => {
+          {NUMBERS.map((number, index) => {
               return (
                 <button 
                   key={ index }
