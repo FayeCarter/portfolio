bd82e4cb6f1bdf0c6e8f5da1549d3dff3702cb85
FayeCarter <fayecarter333@gmail.com>
2020-06-27 11:58:31 +0100

    Refactor number buttons to render from mapped Array

--- src/components/mini-projects/Calculator/Calculator.js
+++ src/components/mini-projects/Calculator/Calculator.js
@@ -8,16 +8,17 @@ function Calculator() {
         className="form" 
       >
         <div className="calc-input" >0</div>
-        <button className="number-buttons" value="1" >1</button>
-        <button className="number-buttons" value="2" >2</button>
-        <button className="number-buttons" value="3" >3</button>
-        <button className="number-buttons" value="4" >4</button>
-        <button className="number-buttons" value="5" >5</button>
-        <button className="number-buttons" value="6" >6</button>
-        <button className="number-buttons" value="7" >7</button>
-        <button className="number-buttons" value="8" >8</button>
-        <button className="number-buttons" value="9" >9</button>
-        <button className="number-buttons" value="0" >0</button>
+        <div className="numbers">
+          {[1,2,3,4,5,6,7,8,9,0].map((number, index) => {
+              return (
+                <button 
+                  key={ index }
+                  className="number-buttons"
+                  value={ number }
+                  >{ number }
+                </button>
+              )})}
+        </div>
       </form>   
     </div>
   );
