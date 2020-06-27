8231fed19ccc7fe8c203f0e3a0549950f01f9cab
FayeCarter <fayecarter333@gmail.com>
2020-06-27 12:42:45 +0100

    Add results field

--- src/components/mini-projects/Calculator/Calculator.js
+++ src/components/mini-projects/Calculator/Calculator.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
-function Calculator() {
+function Calculator(value) {
+
+  const [ display, setDisplay ] = useState("")
+
+  const renderDisplay= (e, number) => {
+    e.preventDefault()
+    setDisplay(number)
+  }
+
   return (
     <div className="Calculator">
       <form 
         className="form" 
       >
-        <div className="calc-input" >0</div>
+        <div className="calc-result" >0</div>
+        <div className="calc-input" >{ display }</div>
         <div className="numbers">
           {[1,2,3,4,5,6,7,8,9,0].map((number, index) => {
               return (
@@ -15,6 +23,7 @@ function Calculator() {
                   key={ index }
                   className="number-buttons"
                   value={ number }
+                  onClick={(event) => { renderDisplay(event, number) }}
                   >{ number }
                 </button>
               )})}
--- src/components/mini-projects/Calculator/__tests__/Calculator.test.js
+++ src/components/mini-projects/Calculator/__tests__/Calculator.test.js
@@ -10,17 +10,31 @@ describe("Navigation component testing", () => {
     wrapper = shallow(<Calculator />);
   })
 
-  test("renders with input field", () => {
-    expect(wrapper.find(".calc-input").exists());
+  test("renders with results field", () => {
+    expect(wrapper.find(".calc-result").exists());
   });
 
   test("input field initially renders with 0", () => {
-    expect(wrapper.find(".calc-input").text()).toBe("0");
+    expect(wrapper.find(".calc-result").text()).toBe("0");
+  });
+
+  test("renders with math input field", () => {
+    expect(wrapper.find(".calc-input").exists());
+  });
+
+  test("math input field initially renders empty", () => {
+    expect(wrapper.find(".calc-input").text()).toBe("");
   });
 
   describe("Number Buttons", () => {
     test("10 buttons are rendered", () => {
       expect(wrapper.children().find(".number-buttons").length).toEqual(10);
+    });
+
+    test("When clicked value is added to cal-input div", () => {
+      const numberButton = wrapper.children().find(".number-buttons").first()
+      numberButton.simulate("click")
+      expect(wrapper.find(".calc-result").text()).toBe(numberButton.prop("value"));
     });
   })
 
