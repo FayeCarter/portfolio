398eb269b57dd6dd98559ecb6be4948779883769
FayeCarter <fayecarter333@gmail.com>
2020-06-29 17:19:04 +0100

    Add Wave Component

---
+++ src/components/mini-projects/Wave/Wave.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Wave() {
+
+  return (
+    <div className="Wave">
+      
+    </div>
+  );
+}
+
+export default Wave;
---
+++ src/components/mini-projects/Wave/__tests__/Wave.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Wave from "../Wave";
+
+import { shallow } from 'enzyme';
+
+describe("Wave component testing", () => {
+  let wrapper;
+
+  beforeEach( () => {
+    wrapper = shallow(<Wave />)
+  })
+
+  test("renders with wave image", () => {
+    expect(wrapper.find(".person").prop()).toBe();
+  });
+
+})
