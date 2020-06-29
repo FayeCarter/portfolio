9999ece9c9eefd8c6dfa0bb2a016aea2258b8c11
FayeCarter <fayecarter333@gmail.com>
2020-06-29 17:33:54 +0100

    Change image on mouse over

--- src/components/mini-projects/Wave/Wave.js
+++ src/components/mini-projects/Wave/Wave.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import wave1 from "./images/wave1.svg"
+import wave2 from "./images/wave2.svg"
 
 function Wave() {
+  const [image, setImage] = useState(wave1)
 
   return (
     <div className="Wave">
-      <img className="person" src={wave1} />
+      <img className="person" src={image} onMouseEnter={() => setImage(wave2)} />
     </div>
   );
 }
--- src/components/mini-projects/Wave/__tests__/Wave.test.js
+++ src/components/mini-projects/Wave/__tests__/Wave.test.js
@@ -11,7 +11,12 @@ describe("Wave component testing", () => {
   })
 
   test("renders with wave image", () => {
-    expect(wrapper.find(".person").prop("className")).toBe("person");
+    expect(wrapper.find(".person").prop("src")).toBe("wave1.svg");
+  });
+
+  test("re-renders with second wave image on hover", () => {
+    wrapper.find(".person").simulate("mouseenter");
+    expect(wrapper.find(".person").prop("src")).toBe("wave2.svg");
   });
 
 })
