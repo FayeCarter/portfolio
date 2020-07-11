bc72075aa46b9982da19e1ba78d643eebf6ece32
FayeCarter <fayecarter333@gmail.com>
2020-07-11 19:12:21 +0100

    Change Class Name if activeNumber === value

--- src/components/mini-projects/Memory/src/components/Block.js
+++ src/components/mini-projects/Memory/src/components/Block.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import "./Block.css"
 
 function Block ({value, activeNumber}) {
 
+  const [ status, setStatus] = useState("inactive");
+
+  useEffect(() => {
+    if( value === activeNumber ) {
+      setStatus("active")
+    }
+  }, [activeNumber]);
+
   return (
-    <div className="block" value={value}>
+    <div className={`block ${status}`} value={value} >
       <div>{value}</div>
     </div>
   );
--- src/components/mini-projects/Memory/src/components/__tests__/Block.test.js
+++ src/components/mini-projects/Memory/src/components/__tests__/Block.test.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { shallow } from 'enzyme';
+import { act, waitForElement } from "@testing-library/react"
+import { shallow, mount } from 'enzyme';
 import Block from "../Block";
 
 describe("Block component testing", () => {
@@ -10,4 +11,4 @@ describe("Block component testing", () => {
     expect(wrapper.prop("value")).toEqual("1");
   });
 
-})
+});
