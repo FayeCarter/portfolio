8a4c429c46b3118c93df86b209ac46f327a22ce6
FayeCarter <fayecarter333@gmail.com>
2020-07-13 14:19:18 +0100

    Fix cardConverter to return Ints

--- src/components/mini-projects/Higher-Lower/src/__tests__/cardConverter.spec.js
+++ src/components/mini-projects/Higher-Lower/src/__tests__/cardConverter.spec.js
@@ -1,17 +1,17 @@
 import { cardConverter } from "../cardCoverter";
 
 test("it converts Jack to value 11", () => {
-  expect(cardConverter("JACK")).toEqual("11");
+  expect(cardConverter("JACK")).toEqual(11);
 });
 
 test("it converts Queen to value 12", () => {
-  expect(cardConverter("QUEEN")).toEqual("12");
+  expect(cardConverter("QUEEN")).toEqual(12);
 });
 
 test("it converts King to value 13", () => {
-  expect(cardConverter("KING")).toEqual("13");
+  expect(cardConverter("KING")).toEqual(13);
 });
 
 test("it converts Ace to value 14", () => {
-  expect(cardConverter("ACE")).toEqual("14");
+  expect(cardConverter("ACE")).toEqual(14);
 });
--- src/components/mini-projects/Higher-Lower/src/cardCoverter.js
+++ src/components/mini-projects/Higher-Lower/src/cardCoverter.js
@@ -1,11 +1,11 @@
 export const cardConverter = (card) => {
   if (card === "JACK") {
-    return "11";
+    return 11;
   } else if (card === "QUEEN") {
-    return "12";
+    return 12;
   } else if (card === "KING") {
-    return "13";
+    return 13;
   } else if (card === "ACE") {
-    return "14";
+    return 14;
   }
 };
