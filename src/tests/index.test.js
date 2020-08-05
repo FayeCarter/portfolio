0f793e945d333b37b0b2aad374fdb4e1f168b544
FayeCarter <fayecarter333@gmail.com>
2020-08-05 16:13:20 +0100

    Update scripts

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -2,8 +2,6 @@ const request = require("supertest");
 const app = require("../../index");
 
 describe("Test the api path", () => {
-
-  afterEach(() => app.close());
   
   test("It should response the GET method", done => {
     request(app)
