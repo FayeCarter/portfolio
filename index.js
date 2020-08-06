d4035ca37e1e5cc2d1f31b2b8c465f97e45995b8
FayeCarter <fayecarter333@gmail.com>
2020-08-06 14:46:49 +0100

    Update scripts

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(express.json());
 
@@ -11,6 +12,9 @@ app.get('/api', cors(), async (req, res) => {
   res.json({ status: 'success', message: 'Running' });
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
