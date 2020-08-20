7c11eec25ad013a31225605b822b3480010b0379
FayeCarter <fayecarter333@gmail.com>
2020-08-20 16:45:03 +0100

    Add basic styling

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navigation from './Navigation';
 import Links from './Links';
+import './Contact.css';
 
 function Contact() {
   return (
@@ -8,27 +9,26 @@ function Contact() {
       <Navigation />
       <Links />
       <div className="Window Contact">
-        <div className="home-details">
-          <from>
-            <label>Name</label>
-            <input 
-              type="text" 
-              name="name"
-              placeholder="Please enter your full name"
-            />
-            <label>Email</label>
-            <input 
-              type="email" 
-              name="email"
-              placeholder="Please enter your email address"
-            />
-            <label>Name</label>
-            <textarea 
-              name="suject"
-              placeholder="Please enter your message here"
-            />
-          </from>
-        </div>
+        <form>
+          <label>Name:</label>
+          <input 
+            type="text" 
+            name="name"
+            placeholder="Please enter your full name"
+          />
+          <label>Email:</label>
+          <input 
+            type="email" 
+            name="email"
+            placeholder="Please enter your email address"
+          />
+          <label>Subject:</label>
+          <textarea 
+            name="suject"
+            placeholder="Please enter your message here"
+          />
+          <button type="submit">Send</button>
+        </form>
       </div>
 
     </div>  
